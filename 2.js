880506ebea533f2bfcfc648e7baf5c350d398275
rulamon <ruben.lamon@gmail.com>
2018-11-29 11:15:47 +0100

    second solution using an array for entities, replacing with map and replace

--- 2.js
+++ 2.js
@@ -2,9 +2,9 @@ function convertHTML(str) {
   const entitiesArr = [
   	[/&/g, "&amp;"],
   	[/</g, "&lt;"],
-  	[">", "&gt;"],
-  	["\"", "&quot;"],
-  	["\'", "&apos;"]
+  	[/>/g, "&gt;"],
+  	[/"/g, "&quot;"],
+  	[/'/g, "&apos;"]
   ];
   entitiesArr.map(x => str = str.replace(x[0], x[1]))
   return str;
